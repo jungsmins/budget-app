f8e5a421567c7c56fdbcf33285d7c955a594b8c9
jungsmins <jungsmins00@gmail.com>
2026-01-29 04:28:49 +0900

    feat: ledger mongoose 스키마 추가

---
+++ server/src/api/ledgers/ledgers.schema.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const ledgerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+      maxlength: [100, 'Name cannot exceed 100 characters'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Description is required'],
+      trim: true,
+      maxlength: [500, 'Description cannot exceed 500 characters'],
+    },
+  },
+  {
+    timestamps: true,
+    collection: 'ledgers',
+  },
+);
+
+ledgerSchema.virtual('id').get(function () {
+  return this._id.toHexString();
+});
+
+ledgerSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    ret.id = ret._id.toHexString();
+    delete ret._id;
+    return ret;
+  },
+});
+
+const Ledger = mongoose.model('Ledger', ledgerSchema);
+
+module.exports = Ledger;

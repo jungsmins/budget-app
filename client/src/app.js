3d0897ee526ef7c6a2e92b436867785e93f789a8
jungsmins <jungsmins00@gmail.com>
2026-02-13 23:37:51 +0900

    refactor: 페이지 요소 생성 방식을 함수 실행으로 변경

--- client/src/app.js
+++ client/src/app.js
@@ -7,9 +7,6 @@ import router from './router';
 const headerEl = header();
 const footerEl = footer();
 
-const homePageEl = homePage();
-const ledgerPageEl = ledgerPage();
-
 function app() {
   const appEl = document.createElement('div');
   appEl.className = 'app';
@@ -22,9 +19,9 @@ function app() {
   appEl.appendChild(pageEl);
   appEl.appendChild(footerEl);
 
-  router.route(homePageEl, '/');
-  router.route(homePageEl, '/ledgers');
-  router.route(ledgerPageEl, '/ledgers/:id');
+  router.route(homePage, '/');
+  router.route(homePage, '/ledgers');
+  router.route(ledgerPage, '/ledgers/:id');
   router.render();
 
   return appEl;
